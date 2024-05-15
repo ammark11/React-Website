548871cfefc29c37f7a5a3123113b3bb34b3f662
ammark11 <ammark11cod7@gmail.com>
2024-05-16 00:14:01 +0400

    Add MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-
-app.listen(3300,() => {
+const mongoURL = 'mongodb://your_username:your_password411416@cluster-1175.my-mongo.com';
+mongooose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+app.listen(3300, () => {
     console.log('Server is upand running on port 3300');
 });
