3436cd5ce6a509f272c7ef5bb303d7f935af7882
ammark11 <ammark11cod7@gmail.com>
2024-05-16 00:29:03 +0400

    Add GET route for products

---
+++ backend/routes/products.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/Product');
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.send(products);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+module.exports = router;
