48a047da473cc092b3be1883483e46a8f1fa8b53
ammark11 <ammark11cod7@gmail.com>
2024-05-16 00:29:45 +0400

    Add POST route for products

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -10,4 +10,14 @@ router.get('/', async (req, res) => {
         res.status(500).send(err);
     }
 });
+
+router.post('/', async (req, res) => {
+    try {
+        const new product = new Product(req.body);
+        await new product.save();
+        res.send(new product);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 module.exports = router;
